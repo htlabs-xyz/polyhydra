0fee9a532e82724b92ba13524601d971f8e49c88
tidvn <tiendung0325@gmail.com>
2025-12-16 23:38:43 +0700

    chore: add next-env.d.ts for TypeScript support in Next.js

---
+++ apps/client/next-env.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+declare module "*.css";
+declare module "@repo/ui/globals.css";
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/app/api-reference/config/typescript for more information.

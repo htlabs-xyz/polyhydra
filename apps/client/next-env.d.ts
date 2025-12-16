20b4bb8f320528a7d6f514b713cd6101e34fa31e
tidvn <tiendung0325@gmail.com>
2025-12-16 23:42:25 +0700

    remove

--- apps/client/next-env.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="next" />
-/// <reference types="next/image-types/global" />
-
-declare module "*.css";
-declare module "@repo/ui/globals.css";
-
-// NOTE: This file should not be edited
-// see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
